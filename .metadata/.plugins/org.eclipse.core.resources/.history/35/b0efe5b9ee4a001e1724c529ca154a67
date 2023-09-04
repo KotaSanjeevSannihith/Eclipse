f34984b00532c54ea6fd789cdf3efb0dffa3c445
KotaSanjeevSannihith <sanjeevsannihith@gmail.com>
2023-09-04 14:27:38 +0530

    Second commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/35/b0efe5b9ee4a001e1724c529ca154a67
@@ -0,0 +1,16 @@
+package classexe;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		int fact=1,num=5,end=1;
+		while(num>=end) {
+			fact*=num;
+			num--;
+		}
+		System.out.println(fact);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ classexe/src/classexe/summation.java
@@ -0,0 +1,17 @@
+package classexe;
+
+public class summation {
+
+	public static void main(String[] args) {
+		int num=5,count=0,end=0;
+		while(num>=end) { //5>=0 5>=1
+			count+=num;   //count=0+5  
+			num--;		  //4
+					  //1
+		}
+		System.out.println(count);
+		// TODO Auto-generated method stub
+
+	}
+
+}
